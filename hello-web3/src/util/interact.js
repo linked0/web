0c516aee63377ba64ea37cad56a6f84500e19a66
Hyunjae Lee <linked0@gmail.com>
2022-10-26 18:41:06 +0900

    Before `Loading your Hello World smart contract`

--- hello-web3/src/util/interact.js
+++ hello-web3/src/util/interact.js
@@ -1,3 +1,7 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
 //export const helloWorldContract;
 
 export const loadCurrentMessage = async () => { 
