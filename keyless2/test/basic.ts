4c772fabfb234ac3e2e8bb0e2cd0af652613a4c8
Hyunjae Lee <linked0@gmail.com>
2023-12-22 10:21:57 +0900

    Add basic.ts and content related to Spaces

---
+++ keyless2/test/basic.ts
@@ -0,0 +1,15 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe.only("Basic Test", function () {
+  describe("EnumerableSet", function () {
+    it("Call contract having EnurableSet", async function () {
+      console.log("Fill me with test code here!");
+    });
+  });
+});
