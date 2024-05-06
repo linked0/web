1674355bb69a70b02d8b9e3b4f8b64dba2713fc5
Hyunjae Lee <linked0@gmail.com>
2024-05-06 13:22:56 +0900

    test husky

--- ex/test/planet.ts
+++ ex/test/planet.ts
@@ -9,11 +9,12 @@ describe("Basic", () => {
       suite: { greeter, basic },
       accounts: { deployer, user },
     } = await loadFixture(deployFullSuiteFixture);
+        console.log("deployer address:", deployer.address);
 
-    console.log("greeter address:", greeter.target);
-    console.log("greeter:", await greeter.greet());
+        console.log("greeter address:", greeter.target);
+        console.log("greeter:", await greeter.greet());
 
-    console.log("basic address:", basic.target);
-    console.log("basic:", await basic.value());
+        console.log("basic address:", basic.target);
+        console.log("basic:", await basic.value());
   });
 });
