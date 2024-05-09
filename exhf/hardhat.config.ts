ae4f70d4b331a963e563c5a2d1ba39ea88591c02
Hyunjae Lee <linked0@gmail.com>
2024-05-09 16:09:24 +0900

    remove unnecessary projects

--- exhf/hardhat.config.ts
+++
@@ -1,9 +0,0 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-foundry";
-
-const config: HardhatUserConfig = {
-  solidity: "0.8.24",
-};
-
-export default config;
