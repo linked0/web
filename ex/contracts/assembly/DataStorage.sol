075301eb6a36e6ce9890e22ff00e0dad95b38090
Hyunjae Lee <linked0@gmail.com>
2024-06-27 21:24:52 +0900

    Make the assembly code run correctly

--- ex/contracts/assembly/DataStorage.sol
+++ ex/contracts/assembly/DataStorage.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "hardhat/console.sol";
+
 /// @title DataStorage contract
 /// @author Jay Lee
 /// @notice This contract is for storing data using assembly and library.
@@ -24,25 +26,27 @@ contract DataStorage {
     bytes32 nameSlot = keccak256(abi.encodePacked(_data.name));
     bytes32 valueSlot = keccak256(abi.encodePacked(_data.name, uint256(0)));
 
-    bytes calldata name = bytes(_data.name);
+    bytes memory name = bytes(_data.name);
+    uint256 nameLength = name.length;
+
     uint256 value = _data.value;
 
     assembly ("memory-safe") {
       // Using keccak256 to get the storage slot
-      let namePtr := add(name.offset, 0x20)
-      let nameLen := 1
+      let namePtr := add(name, 0x20)
+      // let nameLen := 10
 
       // Store name length
-      sstore(nameSlot, nameLen)
+      sstore(nameSlot, nameLength)
 
       // Store the actual string data
-      let dataSlot := add(nameSlot, 0x20)
+      let dataSlot := add(nameSlot, 1)
       for {
         let i := 0
-      } lt(i, nameLen) {
-        i := add(i, 1)
+      } lt(i, nameLength) {
+        i := add(i, 0x20)
       } {
-        sstore(add(dataSlot, mul(i, 0x20)), mload(add(namePtr, mul(i, 0x20))))
+        sstore(add(dataSlot, div(i, 0x20)), mload(add(namePtr, i)))
       }
 
       // Store the value
@@ -61,37 +65,35 @@ contract DataStorage {
     bytes32 valueSlot = keccak256(abi.encodePacked(_name, uint256(0)));
 
     ValueData memory _data;
-    bytes memory gottonName = new bytes(0x20);
+    bytes memory gottonName;
     uint256 value;
     uint256 nameLength;
     assembly {
       // Load the name length from storage
-      let nameLen := sload(nameSlot)
-      nameLength := nameLen
+      nameLength := sload(nameSlot)
 
-      // Allocate memory for the name
-      // let namePtr := mload(0x40)
-      // mstore(namePtr, nameLen)
-      // mstore(0x40, add(namePtr, add(nameLen, 0x20)))
+      // Allocate memory for the gottonName
+      gottonName := mload(0x40)
+      mstore(gottonName, nameLength)
+      mstore(0x40, add(add(gottonName, nameLength), 0x20))
+
       let strPtr := add(gottonName, 0x20)
-      // nameSlot := add(nameSlot, 0x20)
+      let dataSlot := add(nameSlot, 1)
       for {
         let i := 0
-      } lt(i, nameLen) {
-        i := add(i, 1)
+      } lt(i, nameLength) {
+        i := add(i, 0x20)
       } {
-        mstore(
-          add(strPtr, mul(i, 0x20)),
-          sload(add(keccak256(add(nameSlot, 0x20), mul(i, 0x20)), 0x20))
-        )
+        mstore(add(strPtr, i), sload(add(dataSlot, div(i, 0x20))))
       }
       // set value and name
-      // mstore(gottonName, namePtr)
       value := sload(valueSlot)
     }
 
     _data.name = string(gottonName);
     _data.value = value;
+    console.log("nameLength: %d", nameLength);
+    console.log("name: %s, value: %d", _data.name, _data.value);
 
     return _data;
   }
---
+++ ex/test/assembly/data.spec.ts
@@ -0,0 +1,61 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { Wallet } from "ethers";
+import { ethers } from "hardhat";
+
+import { fullSuiteFixture } from "./full-suite.fixture";
+import { hexlify } from "ethers";
+
+describe("DataStorage", function () {
+  const provider = ethers.provider;
+
+  describe.only("Deployment DataStorage", function () {
+    it("Should deploy DataStorage", async function () {
+      const {
+        accounts: { deployer, user },
+        suite: { dataStorage },
+      } = await loadFixture(fullSuiteFixture);
+
+      console.log(dataStorage.target);
+    });
+
+    it("Should set and get the right real value", async function () {
+      const {
+        accounts: { deployer, user },
+        suite: { dataStorage },
+      } = await loadFixture(fullSuiteFixture);
+
+      // Create a ValueData object
+      const valueData = {
+        name: "Example data storage",
+        value: 123456,
+      };
+
+      expect(await dataStorage.setData(valueData)).to.emit(dataStorage, "DataStored").withArgs([valueData.name, valueData.value]);
+      await provider.send("evm_mine", []);
+      expect(await dataStorage.getDataValue(valueData.name)).to.equal(valueData.value);
+    });
+
+    it("Should set and get the right value", async function () {
+      const {
+        accounts: { deployer, user },
+        suite: { dataStorage },
+      } = await loadFixture(fullSuiteFixture);
+
+      // Create a ValueData object
+      const valueData = {
+        name: "Hello World Series Otani! Ryu! Ichiro! Shohei! Kikuchi! Sugano! Yamada! Sasaki*",
+        value: 123456,
+      };
+
+      expect(await dataStorage.setData(valueData)).to.emit(dataStorage, "DataStored").withArgs([valueData.name, valueData.value]);
+      await provider.send("evm_mine", []);
+      const returnData = await dataStorage.getData(valueData.name)
+      console.log(returnData);
+    });
+  });
+});
