847d570a700c7371e46d335be173853a72152774
Hyunjae Lee <linked0@gmail.com>
2024-07-09 20:26:00 +0900

    Add call attack examples

---
+++ ex/contracts/zhacking/call-attack/ICryptoKeeper.sol
@@ -0,0 +1,22 @@
+// SPDX-Licence-Identifier: MIT
+pragma solidity 0.8.24;
+
+interface ICryptoKeeper {
+  function initialize(address[] memory _operators) external;
+
+  function execute(
+    address _contractAddress,
+    bytes calldata _encodedCalldata,
+    uint8 operation
+  ) external returns (bytes memory);
+
+  function executeWithValue(
+    address _contractAddress,
+    bytes calldata _encodedCalldata,
+    uint256 _value
+  ) external payable returns (bytes memory);
+
+  function addOperator(address _address) external;
+
+  function removeOperator(address _address) external;
+}
