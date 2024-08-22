ae126990de87809bbbef57d7405badfb53b0582d
Hyunjae Lee <linked0@gmail.com>
2024-08-22 20:48:01 +0900

    Add planetarium mini project

---
+++ eplanet/scripts/deploy.ts
@@ -0,0 +1,22 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const mytoken = await ethers.deployContract("MyToken");
+  await mytoken.waitForDeployment();
+  console.log(`Deployed MINTABLE_TOKEN: ${mytoken.target}`);
+
+  const delegator = await ethers.deployContract(
+    "TransactionDelegator",
+    [mytoken.target]
+  );
+  await delegator.waitForDeployment();
+
+  console.log(`Deployed TRANSACTION_DELEGATOR_CONTRACT: ${delegator.target}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
