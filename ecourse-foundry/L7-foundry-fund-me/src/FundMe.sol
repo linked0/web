870f3c97ff9f5aad3cff8dcc2e589aa1ff38fdff
Hyunjae Lee <linked0@gmail.com>
2024-11-04 14:17:57 +0900

    modules

--- ecourse-foundry/L7-foundry-fund-me/src/FundMe.sol
+++ ecourse-foundry/L7-foundry-fund-me/src/FundMe.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.19;
 // 2. Imports
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 // 3. Interfaces, Libraries, Contracts
