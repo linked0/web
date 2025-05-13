09b4f2fc6d438182a02406f31e4674f68fc9068e
Hyunjae Lee <linked0@gmail.com>
2025-05-13 15:58:57 +0900

    change rust example

--- exlang/exr/src/main.rs
+++ exlang/exr/src/main.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 struct Config<'config> {
     url: &'config str,
 }
@@ -17,6 +19,18 @@ fn handle<'config, 'req>(handler: &Handler<'config, 'req>) {
 }
 
 fn main() {
+    let mut rng = rand::thread_rng();
+    let n: u8 = rng.gen_range(0..=255);
+    println!("Random number between 1 and 255: {}", n);
+    let f: f64 = rng.gen();
+    println!("Random float between 0 and 1: {}", f);
+    let choice = ["rock", "paper", "scissors"];
+    let pick = choice[rng.gen_range(0..choice.len())];
+    println!("Random choice: {}", pick);
+
+}
+
+fn main2() {
     let config = Config { url: "https://example.com" };
 
     // A block where a request is created.
--- exlang/exr/target/debug/exr.d
+++ exlang/exr/target/debug/exr.d
@@ -1,1 +1,1 @@
-/Users/jay/work/web/exr/target/debug/exr: /Users/jay/work/web/exr/src/main.rs
+/Users/jay/work/web/exlang/exr/target/debug/exr: /Users/jay/work/web/exlang/exr/src/main.rs
